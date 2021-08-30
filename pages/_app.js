048e61993269d34e29e186c8f8549b38c8669b9c
Jargalsaikhan Erdenetsetseg <thejargalsaikhan@gmail.com>
2021-08-30 13:29:50 +0800

    version 1.1.2: _documents.js removed and moved font imports to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,27 @@
-import "../sass/app.scss";
+import Head from "next/head";
 
-// 
 import ThemeLayout from "../layouts/Theme";
 
 import { ThemeContextProvider } from "../store/ThemeContext";
 
+import "../sass/app.scss";
+
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeContextProvider>
+      <Head>
+        {/* IMPORTING TYPOGRAPHY */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400&family=Fira+Sans:wght@300;400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <ThemeLayout>
         <Component {...pageProps} />
       </ThemeLayout>
