c1c240738ab5f41fca6dcae43e3c28f2074ad679
Jargalsaikhan Erdenetsetseg <thejargalsaikhan@gmail.com>
2021-08-31 12:14:16 +0800

    changed theming system

--- layouts/Theme/index.jsx
+++ layouts/Theme/index.jsx
@@ -4,14 +4,11 @@ import { useContext } from "react";
 // IMPORTING THEME CONTEXT FROM STORE
 import ThemeContext from "../../store/ThemeContext";
 
-// IMPORTING BASE STYLES TO THEMIFY BACKGROUND & TEXT COLORS
-import styles from "./Theme.module.scss";
-
 const ThemeLayout = ({ children, ...props }) => {
   const { darkTheme } = useContext(ThemeContext);
   return (
     <div className={darkTheme ? "dark" : "light"}>
-      <div className={styles.base}>{children}</div>
+      {children}
     </div>
   );
 };
