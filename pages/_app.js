fccef8769abcf9a529d0eb7dfb4f1bc664e5f6d0
Jargalsaikhan Erdenetsetseg <thejargalsaikhan@gmail.com>
2022-07-05 02:44:27 +0800

    app js configuration changed

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,30 @@
 import { Fragment } from "react";
-
-import ThemeLayout from "@/layouts/Theme";
-
-import { ThemeContextProvider } from "@/store/ThemeContext";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
+import { SWRConfig } from "swr";
+import { getFetcher } from "@/utils/fetchers";
 
 import "@/sass/app.scss";
 
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
   const Layout = Component.Layout ? Component.Layout : Fragment;
+
   return (
-    <ThemeContextProvider>
-      <ThemeLayout>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeLayout>
-    </ThemeContextProvider>
+    <SessionProvider>
+      <SWRConfig
+        value={{
+          // refreshInterval: 0,
+          revalidateOnFocus: false,
+          fetcher: getFetcher,
+        }}
+      >
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </SWRConfig>
+    </SessionProvider>
   );
 };
 
